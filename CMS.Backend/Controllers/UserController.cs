9f9ea9f60295886863a6dedd0a275ca3a2c6db3a
ngocduy2004 <ngocduy6379@gmail.com>
2026-05-29 16:08:05 +0700

    Hoan thanh buoi 4

--- CMS.Backend/Controllers/UserController.cs
+++ CMS.Backend/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using CMS.Data;
 using CMS.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace CMS.Backend.Controllers
@@ -37,6 +38,75 @@ namespace CMS.Backend.Controllers
             }
 
             return View(user);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            // Kiểm tra xem tên đăng nhập đã tồn tại chưa
+            var checkExist = _context.Users.Any(u => u.Username == model.Username);
+            if (checkExist)
+            {
+                ModelState.AddModelError("Username", "Tên đăng nhập này đã có người dùng!");
+                return View(model);
+            }
+
+            // Lưu User mới vào Database
+            _context.Users.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null) return NotFound();
+
+            return View(user);
+        }
+
+        // POST: Thực hiện lưu thay đổi
+        [HttpPost]
+        public IActionResult Edit(User model, string NewPassword)
+        {
+            // 1. Tìm User gốc trong Database để lấy lại mật khẩu cũ nếu cần
+            var existingUser = _context.Users.AsNoTracking().FirstOrDefault(u => u.Id == model.Id);
+
+            if (existingUser == null) return NotFound();
+
+            // 2. Xử lý mật khẩu: Nếu nhập mới thì lấy cái mới, nếu trống thì lấy cái cũ
+            if (!string.IsNullOrEmpty(NewPassword))
+            {
+                model.PasswordHash = NewPassword; // Sau này sẽ mã hóa tại đây
+            }
+            else
+            {
+                model.PasswordHash = existingUser.PasswordHash;
+            }
+
+            // 3. Cập nhật vào Database
+            _context.Users.Update(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
